e92b7c41db34e0f244ccce2e93b3a0be4306b8c6
salimraza786 <razasalimraza17@gmail.com>
2023-10-13 20:40:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var main = document.querySelector("#main")
+var curs = document.querySelector(".cursor")
+
+main.addEventListener("mousemove",function(dets){
+   curs.style.left = dets.x +"px"
+   curs.style.top = dets.y +"px"
+})
